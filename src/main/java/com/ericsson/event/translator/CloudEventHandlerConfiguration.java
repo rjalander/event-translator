9d5d82a9df17f1b96816f9daefcb22776648b14c
Jalander Ramagiri <jalander.ramagiri@est.tech>
2022-08-08 17:12:25 +0100

    adding CloudEventHandlerConfiguration

---
+++ src/main/java/com/ericsson/event/translator/CloudEventHandlerConfiguration.java
@@ -0,0 +1,18 @@
+package com.ericsson.event.translator;
+
+import io.cloudevents.spring.mvc.CloudEventHttpMessageConverter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class CloudEventHandlerConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(0, new CloudEventHttpMessageConverter());
+    }
+}
